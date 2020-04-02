25daaa5b88cc4baf913eb7d9a4e7f04a9e015bd1
mao <maoyuan@gmail.com>
2020-04-02 14:46:38 -0400

    init

---
+++ src/test/java/com/maotion/microeureka/MicroeurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maotion.microeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroeurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
